193a8b86b2a9a39401448ca62a4c1759e941519c
unknown <islammohamedahmed49@gmail.com>
2024-04-15 18:38:18 +0200

    paj

---
+++ PetyaAndJava/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+long n;
+
+try
+{
+    n = ToInt64(ReadLine());
+
+    if (n <= 127)
+    {
+        WriteLine("byte");
+    }
+    else if(n <= 32767) {
+        WriteLine("short");
+    }
+    else if(n<= 2147483647)
+    {
+        WriteLine("int");
+    }
+    else
+    {
+        WriteLine("long");
+    }
+}
+catch(Exception e)
+{
+    WriteLine("BigInteger");
+}
